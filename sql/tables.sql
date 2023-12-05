940f55f52d77704639a8a73bf83e5f46b7ddad9b
light <dracokd19@gmail.com>
2023-12-05 17:16:31 +0545

    updated table

--- sql/tables.sql
+++ sql/tables.sql
@@ -4,4 +4,11 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `phone` varchar(10) NOT NULL
-)
+);
+
+CREATE TABLE `admins` (
+  admin_id INT PRIMARY KEY AUTO_INCREMENT,
+  a_uname VARCHAR(255) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+);
