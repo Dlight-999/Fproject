47945d9e2c611ed16c90c6ef355ba804831d0775
light <light@localhost.localdomain>
2023-12-05 20:29:16 +0545

    added constasnt

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors',1);
+
+
+    session_start();
+    ob_start();
+    define('siteurl','http://localhost/Fproject/');
+    define('LOCALHOST','localhost');
+    define('db_uname','root');
+    define('db_pass','');
+    define('db_name','Fproject');
+
+?>
