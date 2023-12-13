1bc08badcdce16bf982bf0110fc19619548da3e1
light <dracokd19@gmail.com>
2023-12-14 02:47:08 +0545

    idk

--- user/hero.php
+++ user/hero.php
@@ -1,39 +1,30 @@
  
   
-    <div class="container col-xxl-8 px-4 py-5">
-	<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-		<div class="col-10 col-sm-8 col-lg-6">
-			<img src="https://img.freepik.com/premium-photo/modern-hotel-room-interior-nature-view-outside-room_1000320-951.jpg" class="d-block mx-lg-auto img-fluid" alt="" loading="lazy">
+    <div lc-helper="background" class="container-fluid py-5 mt-5  d-flex justify-content-center" style="  background-image: url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1124&amp;q=100');
+    background-position: center;
+background-size:cover;
+background-repeat:no-repeat">
+	<div class="p-5 mb-4 lc-block col-xxl-7 col-lg-8 col-12" style=" backdrop-filter: blur(6px) saturate(102%);
+    -webkit-backdrop-filter: blur(6px) saturate(102%);
+    background-color: rgba(255, 255, 255, 0.45);
+    border-radius: 12px;
+    border: 1px solid rgba(209, 213, 219, 0.3);">
+		<div class="lc-block">
+			<div editable="rich">
+				<h2 class="fw-bolder display-3">Custom jumbotron</h2>
+			</div>
 		</div>
-		<div class="col-lg-6">
-			<div class="lc-block mb-3">
-				<div editable="rich">
-					<h2 class="fw-bold display-5">Brand Name</h2>
-					<h3>Where Every Stay Feels Like a Dream Getaway!
-</h3>
-				</div>
+		<div class="lc-block col-md-8">
+			<div editable="rich">
+				<p class="lead">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.
+				</p>
 			</div>
-
-			<div class="lc-block mb-3">
-				<div editable="rich">
-					<p class="lead">Indulge in Unparalleled Comfort and Luxury
-Discover a haven of tranquility and sophistication
-that goes beyond the ordinary.
-
-Book Your Perfect Escape Today
-and Experience the Pinnacle of Hospitality.
-
-Your Journey Starts Here.
-					</p>
-				</div>
-			</div>
-
-			<div class="lc-block d-grid gap-2 d-md-flex justify-content-md-start">
-				<a class="btn btn-primary px-4 me-md-2" href="#" role="button">Book Now</a>
-			</div>
-
+		</div>
+		<div class="lc-block">
+			<a class="btn btn-dark" href="#" role="button">Click me, I'm a button</a>
 		</div>
 	</div>
-</div> 
+</div>
+ 
  
   
